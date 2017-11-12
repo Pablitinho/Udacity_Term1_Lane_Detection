9a768d42b8feea9b6e85d44de1e5d7fc11af2819
LAPTOP-HN0T3FIJ\pablo <pablogsanchez@gmail.com>
2017-11-12 18:31:41 +0100

    More stable code, need parameter changes to work properly in the challenge video. Also I need to port the code to Jupyter Notebook

--- line_detection.py
+++ line_detection.py
@@ -5,6 +5,9 @@ import numpy as np
 import cv2
 import math
 import os
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+import imageio as imio
 # --------------------------------------------------------------------------------
 def grayscale(img):
     """Applies the Grayscale transform
@@ -78,9 +81,151 @@ def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
     """
     lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len,
                             maxLineGap=max_line_gap)
+    return lines
+# --------------------------------------------------------------------------------
+def get_scores(image, lines, num_lines,lines_equations,min_y):
+
+    score = np.zeros(len(lines))
+    new_coordinates = np.zeros(2)
+
+    for i in range(num_lines):
+        equation = lines_equations[i]
+        score[i] = 0
+        if equation[0] != 0 and math.isinf(equation[0]) == False and math.isinf(equation[1]) == False:
+            for y in range(min_y, image.shape[1], 1):
+
+                if equation[0] == 0:
+                    new_coordinates[0] = equation[1]
+                else:
+                    new_coordinates[0] = int(round((y - equation[1]) / (equation[0]+0.0001)))
+                new_coordinates[1] = int(round(y))
+                for x in range(-5, 5, 1):
+                    y_ = int(new_coordinates[1])
+                    x_ = int(new_coordinates[0] + x)
+                    if x_ > 0 and x_< image.shape[1] and y_ > 0 and y_< image.shape[0]:
+                        if image[y_, x_] > 0:
+                            score[i] = score[i] + 1
+
+    return score
+# --------------------------------------------------------------------------------
+def get_line_equation(line):
+
+    line_equation = np.zeros(2)
+
+    xA = line[0, 0]
+    yA = line[0, 1]
+    xB = line[0, 2]
+    yB = line[0, 3]
+    line_equation[0] = (yA - yB)/(xA - xB) # Slope
+    if line_equation[0] == 0:
+        pp = 0
+
+
+    line_equation[1] = yA - line_equation[0]*xA #Bias
+    return line_equation
+# --------------------------------------------------------------------------------
+def new_coordinate_line(image,line_equation,min_y):
+
+    new_coordinates = np.zeros(4)
+
+    if line_equation[0] == 0:
+        new_coordinates[0] = line_equation[1]
+    else:
+        new_coordinates[0] = (min_y - line_equation[1]) / (line_equation[0]+0.0001)
+
+    new_coordinates[1] = min_y
+
+    if line_equation[0] == 0:
+        new_coordinates[0] = line_equation[1]
+    else:
+        new_coordinates[2] = (image.shape[1] - line_equation[1]) / (line_equation[0]+0.0001)
+    new_coordinates[3] = image.shape[1]
+
+    return new_coordinates
+# --------------------------------------------------------------------------------
+def get_best_line_score(lines,num_lines,score):
+
+    best_score_idx = -1
+    best_score = -1
+
+    for i in range(num_lines):
+        if score[i]>best_score:
+            best_score = score[i]
+            best_score_idx = i
+
+    return lines[best_score_idx], best_score_idx
+# --------------------------------------------------------------------------------
+def detect_left_right_line(img,lines):
+
+    min_y = int(img.shape[1]/3)
+    l_r_lines = np.zeros([1,2, 4])
+    l_r_lines = l_r_lines.astype(int)
+
+    left_score = np.zeros(len(lines))
+    right_score = np.zeros(len(lines))
+    left_candidates = np.copy(lines)*0
+    num_left_candidates = 0
+    right_candidates = np.copy(lines)*0
+    num_right_candidates = 0
+
+    left_line_equation = np.zeros([len(lines), 2])
+    right_line_equation = np.zeros([len(lines), 2])
+
+    # Get the center of the image, in the proper way it should be the Y vehicle coordinates respect to the camera
+    center_x = (img.shape[1])/2
+
+    for i in range(len(lines)):
+        line = lines[i]
+
+        x_coordinate = 0
+
+        if line[0, 1] > line[0, 3]:
+            x_coordinate = line[0, 0]
+        else:
+            x_coordinate = line[0, 2]
+
+        if x_coordinate >= center_x:
+            line_equation = get_line_equation(line)
+            right_line_equation[num_right_candidates] = line_equation
+
+            right_candidates[num_right_candidates] = lines[i]
+            #
+
+            num_right_candidates = num_right_candidates+1
+        else:
+            line_equation = get_line_equation(line)
+            left_line_equation[num_left_candidates] = line_equation
+
+            left_candidates[num_left_candidates] = lines[i]
+
+            #left_candidates[num_left_candidates] = new_coordinate_line(img, line_equation, min_y)
+
+            num_left_candidates = num_left_candidates + 1
+
+    left_score = get_scores(img, left_candidates, num_left_candidates-1, left_line_equation, min_y)
+    right_score = get_scores(img, right_candidates, num_right_candidates-1, right_line_equation, min_y)
+
+    l_r_lines[0, 0], best_score_left = get_best_line_score(left_candidates, num_left_candidates-1, left_score)
+    l_r_lines[0, 1], best_score_right = get_best_line_score(right_candidates, num_right_candidates-1, right_score)
+
+    l_r_lines[0, 0] = new_coordinate_line(img, left_line_equation[best_score_left], min_y)
+    l_r_lines[0, 1] = new_coordinate_line(img, right_line_equation[best_score_right], min_y)
+
+    return l_r_lines
+# --------------------------------------------------------------------------------
+def hough_lines_road(img, rho, theta, threshold, min_line_len, max_line_gap):
+    """
+    `img` should be the output of a Canny transform.
+
+    Returns an image with hough lines drawn.
+    """
+    lines = hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap)
+
+    l_r_lines = detect_left_right_line(img, lines)
+
     line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
-    draw_lines(line_img, lines)
-    return line_img, lines
+    draw_lines(line_img, l_r_lines)
+    return line_img, l_r_lines
 # --------------------------------------------------------------------------------
 def weighted_img(img, initial_img, α=0.8, β=1., λ=0.):
     """
@@ -96,7 +241,7 @@ def weighted_img(img, initial_img, α=0.8, β=1., λ=0.):
     """
     return cv2.addWeighted(initial_img, α, img, β, λ)
 
-def detect_lines_on_road(image,kernel_size = 5, low_threshold = 50, high_threshold = 150, rho = 2, threshold = 40, min_line_length = 50, max_line_gap = 20):
+def detect_lines_on_road(image,kernel_size = 5, low_threshold = 50, high_threshold = 150, rho = 2, threshold = 40, min_line_length = 50, max_line_gap = 30):
 
     # Convert to Gray scale
     gray = grayscale(image)
@@ -123,37 +268,52 @@ def detect_lines_on_road(image,kernel_size = 5, low_threshold = 50, high_thresho
 
     # Run Hough on edge detected image
     # Output "lines" is an array containing endpoints of detected line segments
-    lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),
-                            min_line_length, max_line_gap)
+    hough_image, lines = hough_lines_road(masked_edges, rho, theta, threshold, min_line_length, max_line_gap)
 
-    hough_image, lines = hough_lines(masked_edges, rho, theta, threshold, min_line_length, max_line_gap)
-
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     # Draw the lines on the edge image
-    lines_edges = weighted_img(hough_image, image, 0.8, 1., 0.)
+    lines_edges = weighted_img(hough_image, image, 0.9, 1., 0.)
 
-    # Slop
-    m = np.zeros(len(lines))
-
-    cv2.imshow('hough_image', lines_edges)
-    cv2.waitKey(0)
-
+    return lines_edges
 # --------------------------------------------------------------------------------
-
+# Read the images in the folder test_image.
 image_list = os.listdir("test_images/")
-
-print('Images:', image_list)#reading in an image
-image = mpimg.imread('test_images/solidWhiteRight.jpg')
-
-#printing out some stats and plotting
-# print('This image is:', type(image), 'with dimensions:', image.shape)
-# plt.imshow(image)  # if you wanted to show a single color channel image called 'gray', for example, call as plt.imshow(gray, cmap='gray')
-# plt.show()
-kernel_size = 5
-low_threshold = 50
-high_threshold = 150
+# --------------------------------------------------------------------------------
+# Set the parameters to detect the lines.
+# --------------------------------------------------------------------------------
+kernel_size = 5 #Gaussina kernel size
+low_threshold = 50 # low thr in the canny method
+high_threshold = 150 # high thr in canny method
 rho = 2  # distance resolution in pixels of the Hough grid
 threshold = 40  # minimum number of votes (intersections in Hough grid cell)
 min_line_length = 50  # minimum number of pixels making up a line
 max_line_gap = 30  # maximum gap in pixels between connectable line segments
+# --------------------------------------------------------------------------------
 
-detect_lines_on_road(image, kernel_size, low_threshold, high_threshold, rho, threshold, min_line_length, max_line_gap)
+# --------------------------------------------------------------------------------
+# Loop to process all the images.
+# --------------------------------------------------------------------------------
+for i in range(len(image_list)):
+
+    print('Image:', image_list[i])  # reading in an image
+    image = mpimg.imread('test_images/' + image_list[i])
+    image_lines = detect_lines_on_road(image, kernel_size, low_threshold, high_threshold, rho, threshold,
+                                       min_line_length, max_line_gap)
+
+    cv2.imshow('Lane Detection', image_lines)
+    cv2.waitKey(0)
+
+video_list = os.listdir("test_videos/")
+for i in range(len(video_list)):
+
+    imio.plugins.ffmpeg.download()
+    white_output = 'test_videos_output/' + video_list[i]
+    clip1 = VideoFileClip('test_videos/' + video_list[i])
+    white_clip = clip1.fl_image(detect_lines_on_road) #NOTE: this function expects color images!!
+    white_clip.write_videofile(white_output, audio=False)
+
+    HTML("""
+    <video width="960" height="540" controls>
+    <source src="{0}">
+    </video>
+    """.format(white_output))
